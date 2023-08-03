4946e06f1251d92f203b908f05cf8c9f95363792
JebGy <martinmunayco123@hotmail.com>
2023-08-03 10:44:30 -0500

    Update S1D4V2
    Updating LoginForm

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -4,7 +4,7 @@ import LoginForm from "@/components/loginComps/LoginForm";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-4 w-screen h-screen">
+    <div className="grid grid-cols-1 lg:grid lg:grid-cols-4 md:grid md:grid-cols-4 w-screen h-screen">
       <div className="col-span-2  flex flex-col items-center justify-center h-full w-full">
         <h2 className="font-semibold text-4xl">
           EMM - ERP
