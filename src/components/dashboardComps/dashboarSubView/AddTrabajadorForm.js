4aa43f6e9586c1353cdacd58c94ee1a02dfb0b7a
JebGy <martinmunayco123@hotmail.com>
2023-08-07 11:07:47 -0500

    Update S2D1V3
    Corrigiendo modal

--- src/components/dashboardComps/dashboarSubView/AddTrabajadorForm.js
+++ src/components/dashboardComps/dashboarSubView/AddTrabajadorForm.js
@@ -445,7 +445,7 @@ export default function AddTrabajadorForm() {
                   </svg>
                 </button>
               </div>
-              <div className="flex flex-col gap-5 h-4/6 overflow-y-scroll">
+              <div className="flex flex-col gap-5 h-5/6 overflow-y-scroll">
                 {load ? (
                   reportes.map((reporte) => {
                     return (
