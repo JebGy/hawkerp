00c888273128f5152f96019989ff673d4a9fe0c0
JebGy <martinmunayco123@hotmail.com>
2023-08-14 09:48:02 -0500

    Update S2D5V4
    Algoritmo de compresión de imagen

--- src/components/trabajadorComps/ReportItem.js
+++ src/components/trabajadorComps/ReportItem.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 
 function ReportItem({ url, actividad, hora }) {
   const [image, setImage] = useState("");
+  const [view, setView] = useState(false);
   useEffect(() => {
     console.log("ReportItem");
   }, []);
@@ -22,19 +23,47 @@ function ReportItem({ url, actividad, hora }) {
         <h3 className="text-lg">{actividad}</h3>
       </div>
       {image || url === "" ? (
-        <Image
-          src={
-            image
-              ? image
-              : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
-          }
-          alt="Picture of the author"
-          width={250}
-          height={250}
-          className="rounded-lg w-full h-full object-cover"
-        />
+        <div className="w-full h-full">
+          <Image
+            onClick={() => {
+              setView(!view);
+            }}
+            src={
+              image
+                ? image
+                : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+            }
+            alt="Picture of the author"
+            width={250}
+            height={250}
+            className="rounded-lg w-full h-full object-cover"
+          />
+        </div>
       ) : (
         <p>Loading...</p>
+      )}
+      {view && (
+        <div
+          onClick={() => {
+            setView(!view);
+          }}
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[90]"
+        >
+          <div className="w-1/2 h-1/2 bg-white rounded-lg">
+            <Image
+              src={
+                image
+                  ? image
+                  : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+              }
+              alt="Picture of the author"
+              width={2500}
+              height={800}
+              priority={true}
+              className="rounded-lg w-full h-96 object-cover"
+            />
+          </div>
+        </div>
       )}
     </div>
   );
