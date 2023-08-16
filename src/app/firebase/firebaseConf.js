347337f10652b01ee1bf2c44fdcf238b29a79df7
JebGy <martinmunayco123@hotmail.com>
2023-08-16 10:23:22 -0500

    Update S3D3V1
    Implementación de generación de reporte markdown

--- src/app/firebase/firebaseConf.js
+++ src/app/firebase/firebaseConf.js
@@ -47,7 +47,7 @@ export const dowloadFile = async (urlImage) => {
     const url = await getDownloadURL(storageRef);
     return url;
   } catch (error) {
-    return "";
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
   }
 };
 
--- src/components/dashboardComps/dashboarSubView/AddTrabajadorForm.js
+++ src/components/dashboardComps/dashboarSubView/AddTrabajadorForm.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { app } from "@/app/firebase/firebaseConf";
+import { createReportMd, createReportPdf } from "@/app/service/FileExport";
 import ReportItem from "@/components/trabajadorComps/ReportItem";
 import {
   addDoc,
@@ -195,7 +196,6 @@ export default function AddTrabajadorForm() {
               <button
                 onClick={(e) => {
                   e.preventDefault();
-                  
                 }}
                 className="bg-gradient-to-r from-red-500 to-orange-400 text-white rounded-full w-full p-2 hover:shadow-xl hover:shadow-yellow-500 transition-all active:scale-95"
               >
@@ -467,33 +467,61 @@ export default function AddTrabajadorForm() {
                             >
                               {reporte.id}
                             </h1>
-                            <button
-                              className="bg-purple-500 hover:bg-purple-600 text-white font-bold p-2 rounded-full transition-all active:scale-95 flex flex-row items-center justify-center gap-5 w-fit"
-                              onClick={() => {
-                                setReportTosee(reporte.id);
-                                setCanSeeReportes(!canSeeReportes);
-                              }}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-6 h-6"
+                            <div className="flex flex-row items-center justify-center gap-5">
+                              <button
+                                onClick={() => {
+                                  if (typeof window !== "undefined") {
+                                    createReportMd(
+                                      reporte.data(),
+                                      trabajadorEdit
+                                    );
+                                  }
+                                }}
+                                className="bg-zinc-500 p-2 rounded-full bg-opacity-20 active:scale-95 transition-all"
                               >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                                />
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                              </svg>
-                            </button>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="w-6 h-6"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                                  />
+                                </svg>
+                              </button>
+                              <button
+                                className="bg-purple-500 hover:bg-purple-600 text-white font-bold p-2 rounded-full transition-all active:scale-95 flex flex-row items-center justify-center gap-5 w-fit"
+                                onClick={() => {
+                                  setReportTosee(reporte.id);
+                                  setCanSeeReportes(!canSeeReportes);
+                                }}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="w-6 h-6"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                  />
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                  />
+                                </svg>
+                              </button>
+                            </div>
                           </div>
                           {localStorage.setItem(
                             "reporte",
@@ -511,7 +539,7 @@ export default function AddTrabajadorForm() {
                                 ) : null
                               }
                               {reporte.data().lista.map((tarea) => {
-                                return (
+                                tarea ? (
                                   <ReportItem
                                     url={tarea.imagenurl ? tarea.imagenurl : ""}
                                     key={
@@ -522,7 +550,7 @@ export default function AddTrabajadorForm() {
                                     }
                                     hora={tarea.hora ? tarea.hora : ""}
                                   />
-                                );
+                                ) : null;
                               })}
                             </div>
                           ) : null}
